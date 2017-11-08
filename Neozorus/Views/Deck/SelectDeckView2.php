acf1dfa1568080d768b5c8dd07f25545886d65cb
JGitit <jordi.sanchez89@gmail.com>
2017-11-08 09:57:21 +0100

    HTML+CSS Deck Jordi

---
+++ Neozorus/Views/Deck/SelectDeckView2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Neozorus</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../../assets/css/SelectDeck2.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+	<div id="mainContainer">
+
+		<div id="marginContainer">
+
+			<div id="logo">
+				<img src="../../assets/img/logoNeozorus.png">
+			</div>
+
+			<div id="action">
+
+				<div id="creer">
+					<a href="">Créer un deck</a>
+				</div>
+
+				<div id="t_rex">
+					<img src="../../assets/img/headshot_rex.png">
+				</div>
+
+				<div id="modifier">
+					<a href="">Changer de Héro</a>
+				</div>
+
+			</div>
+
+			<div id="decksExistants">
+				<p>Mes Decks</p>
+			</div>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
