fe98b57288b6928aa325bf6b492b00009c3d1465
JGitit <jordi.sanchez89@gmail.com>
2017-11-08 10:18:18 +0100

    Header a intégrer

---
+++ Neozorus/Views/Deck/divDeck.php
@@ -0,0 +1,27 @@
+<div id="headerDecks">
+
+	<div id="logo">
+		<img src="../../assets/img/logoNeozorus.png">
+	</div>
+
+	<div id="action">
+
+		<div id="creer">
+			<a href="">Créer un deck</a>
+		</div>
+
+		<div id="t_rex">
+			<img src="../../assets/img/headshot_rex.png">
+		</div>
+
+		<div id="modifier">
+			<a href="">Changer de Héro</a>
+		</div>
+
+	</div>
+
+	<div id="decksExistants">
+		<p>Mes Decks</p>
+	</div>
+
+</div>
