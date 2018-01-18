36ab595baa93974fc33ed0e9443428168f043f10
Arnaud LG <arnaud.lgrd@gmail.com>
2018-01-18 11:24:27 +0100

    Connexion: Mise en forme de la page de Connexion
    Modification et ajustement du css
    Responsive
    Correction de la vérification de session déjà ouverte

--- Neozorus/Views/User/ConnexionInscription.php
+++ Neozorus/Views/User/ConnexionInscription.php
@@ -2,7 +2,7 @@
 		<a href=".?controller=user&action=inscription" title="inscription"><div class="btn">Inscription</div></a>
 		<a href=".?controller=user&action=connexion" title="connexion"><div class="btn">Connexion</div></a>
 		<section class="sous-menu">
-			<a href=".?controller=carte&action=afficherCollectionCarte" title="Cartes"><div class="btn">Cartes</div></a>
+			<a href=".?controller=carte&action=afficherCollectionCarte" title="Voir les cartes"><div class="btn">Cartes</div></a>
 			<a href=".?controller=home&action=affichagePageRegles" title="Règles du jeu"><div class="btn">Règles</div></a>
 		</section>
 	</nav>
