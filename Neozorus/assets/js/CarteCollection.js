777721c7ec4c6df68815d787f4fc6d819cc14d6c
Arnaud LG <arnaud.lgrd@gmail.com>
2018-01-23 17:01:03 +0100

    Home: style des boutons de sélection des filtres
    Animations au survol
    Styles au click et à la sélection

--- Neozorus/assets/js/CarteCollection.js
+++ Neozorus/assets/js/CarteCollection.js
@@ -18,13 +18,46 @@ $(function(){
 
 		});
 
-	// fonction de chargement des cartes en fonction des paramtres sélectionnés
+	// fonction de chargement des cartes en fonction des paramètres sélectionnés
 	function loadFilteredView(){
 		let parameters = '';
-		$('#filter').children().find('.selected').each(function(){
+		
+		$('#filter').find('.selected').each(function(){
 			parameters += '&' + $(this).parent().attr('id') + '=' + $(this).attr('data');
 		});
 		$('.affichageCarte').load('.?controller=carte&action=afficherCollectionCarte&ajax=1'+parameters);
+
+		styleFilters();
 	}
 
+	function styleFilters(){
+
+		$('#type').children().each(function(){
+			let src = $(this).attr('src');
+			let regex = new RegExp('(.*_)[123](\.png)', 'i');
+			if($(this).hasClass('selected')){
+				$(this).attr('src', src.replace(regex, '$1'+1+'$2'));
+			}else{
+				$(this).attr('src', src.replace(regex, '$1'+3+'$2'));
+			}
+		});
+
+		$('#type').children().mousedown(function(){
+			let src = $(this).attr('src');
+			let regex = new RegExp('(.*_)[123](\.png)', 'i');
+
+			$(this).attr('src', src.replace(regex, '$1'+2+'$2'));			
+		});
+
+		$('#mana').children().each(function(){
+			let src = $(this).find('img').attr('src');
+			let regex = new RegExp('(.*pilluleBleue).*(\.png)', 'i');
+
+			if($(this).hasClass('selected')){
+				$(this).find('img').attr('src', src.replace(regex, '$1$2'));
+			}else{
+				$(this).find('img').attr('src', src.replace(regex, '$1Vide$2'));
+			}
+		});
+	}
 });
